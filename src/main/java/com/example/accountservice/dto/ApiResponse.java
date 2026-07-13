559aacec8b266a36c5deb00976aa5d7241e702e7
rayana44 <96428403+rayana44@users.noreply.github.com>
2026-07-13 22:02:38 +0530

    Create ApiResponse.java

---
+++ src/main/java/com/example/accountservice/dto/ApiResponse.java
@@ -0,0 +1,41 @@
+package com.example.accountservice.dto;
+
+public class ApiResponse<T> {
+
+    private String message;
+    private int status;
+    private T data;
+
+    public ApiResponse() {
+    }
+
+    public ApiResponse(String message, int status, T data) {
+        this.message = message;
+        this.status = status;
+        this.data = data;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
