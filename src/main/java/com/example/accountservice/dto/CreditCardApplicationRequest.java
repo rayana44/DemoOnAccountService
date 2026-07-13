52a38b6bbb21efda2f3699566991dab172fcb5dc
rayana44 <96428403+rayana44@users.noreply.github.com>
2026-07-13 22:05:05 +0530

    Create CreditCardApplicationRequest.java

---
+++ src/main/java/com/example/accountservice/dto/CreditCardApplicationRequest.java
@@ -0,0 +1,41 @@
+package com.example.accountservice.dto;
+
+public class CreditCardApplicationRequest {
+
+    private String accountNumber;
+    private String customerName;
+    private String email;
+    private String phoneNumber;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
